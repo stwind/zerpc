f935f28f6d9494c72c058ce1050cb785fb7a6066
stwind <stwindfy@gmail.com>
2013-05-04 15:15:28 +0800

    Add req meta support

--- src/zerpc_req.erl
+++ src/zerpc_req.erl
@@ -3,16 +3,16 @@
 -export([new/2]).
 -export([body/1]).
 -export([reply/2]).
--export([start_time/1]).
--export([end_time/1]).
+-export([meta/2]).
+-export([meta/3]).
+-export([set_meta/3]).
 
 -record(req, {
         peer :: binary(),
         msg :: binary(),
         body :: term(),
         resp :: binary(),
-        start_time = os:timestamp() :: erlang:timestamp(),
-        end_time :: erlang:timestamp()
+        meta = dict:new() :: dict()
     }).
 
 %% ===================================================================
@@ -20,7 +20,8 @@
 %% ===================================================================
 
 new(Peer, Msg) ->
-    #req{peer = Peer, msg = Msg}.
+    Req = #req{peer = Peer, msg = Msg},
+    set_meta(start_time, os:timestamp(), Req).
 
 body(#req{body = undefined, msg = Msg} = Req) ->
     Body = parse_msg(Msg),
@@ -29,15 +30,21 @@ body(Req) ->
     {g(body, Req), Req}.
 
 reply(Reply, #req{peer = Peer} = Req) ->
-    Req1 = Req#req{end_time = os:timestamp()},
+    Req1 = set_meta(end_time, os:timestamp(), Req),
     zerpc_server:reply(Peer, Reply),
     Req1.
 
-start_time(Req) ->
-    {g(start_time, Req), Req}.
+meta(Key, Req) ->
+    meta(Key, Req, undefined).
 
-end_time(Req) ->
-    {g(end_time, Req), Req}.
+meta(Key, #req{meta = Meta}, Default) ->
+    case dict:find(Key, Meta) of
+        {ok, Val} -> Val;
+        error -> Default
+    end.
+
+set_meta(Key, Val, #req{meta = Meta} = Req) ->
+    Req#req{meta = dict:store(Key, Val, Meta)}.
 
 %% ===================================================================
 %% Public
@@ -52,6 +59,4 @@ parse_msg(Msg) ->
     end.
 
 g(peer, #req{peer = Peer}) -> Peer;
-g(body, #req{body = Body}) -> Body;
-g(start_time, #req{start_time = Body}) -> Body;
-g(end_time, #req{end_time = Body}) -> Body.
+g(body, #req{body = Body}) -> Body.
