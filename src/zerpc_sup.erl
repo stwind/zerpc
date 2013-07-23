86ae17c6f2874184f661e2c77d430462d5aa4ca2
stwind <stwindfy@gmail.com>
2013-07-23 12:14:13 +0800

    Support client and server mode at same time

--- src/zerpc_sup.erl
+++ src/zerpc_sup.erl
@@ -34,51 +34,62 @@ init([Context]) ->
 %% ===================================================================
 
 children(Context) ->
-    case zerpc_util:get_env(mode, client) of
-        server ->
-            [server(Context), router(), worker_pool()];
-        client ->
-            lists:flatten([clients(Context)])
+    server_pool(Context) ++ client_pool(Context).
+
+server_pool(Context) ->
+    case zerpc_util:get_env(server) of
+        undefined ->
+            [];
+        ServerOpts ->
+            [server(Context, ServerOpts), router(), worker_pool(ServerOpts)]
     end.
 
-clients(Context) ->
-    Pools = zerpc_util:get_env(pools, []),
+client_pool(Context) ->
+    case zerpc_util:get_env(client) of
+        undefined ->
+            [];
+        ClientOpts ->
+            lists:flatten([clients(Context, ClientOpts)])
+    end.
+
+clients(Context, Opts) ->
+    Pools = proplists:get_value(pools, Opts, []),
     [client(Context, P) || P <- Pools].
 
-client(Context, {Name, Options}) ->
-    Size = proplists:get_value(size, Options),
+client(Context, {Name, Opts}) ->
+    Size = proplists:get_value(size, Opts),
     PoolArgs = [
         {name, {local, Name}},
         {worker_module, zerpc_client},
         {size, Size},
-        {max_overflow, proplists:get_value(overflow, Options, Size * 3)}
+        {max_overflow, proplists:get_value(overflow, Opts, Size * 3)}
     ],
     WorkerArgs = [
         {context, Context},
-        {endpoint, proplists:get_value(endpoint, Options, "tcp://127.0.0.1:5556")},
-        {send_timeout, zerpc_util:get_env(send_timeout, 5000)},
-        {recv_timeout, zerpc_util:get_env(recv_timeout, 5000)}
+        {endpoint, proplists:get_value(endpoint, Opts, "tcp://127.0.0.1:5556")},
+        {send_timeout, proplists:get_value(send_timeout, Opts, 5000)},
+        {recv_timeout, proplists:get_value(recv_timeout, Opts, 5000)}
     ],
     Id = list_to_atom("zerpc_woker_pool_" ++ atom_to_list(Name)),
     poolboy:child_spec(Id, PoolArgs, WorkerArgs).
 
-server(Context) ->
-    Endpoint = zerpc_util:get_env(endpoint, "tcp://*:5556"),
-    {zerpc_server, {zerpc_server, start_link, [Endpoint, Context]}, permanent,
-        5000, worker, [zerpc_server]}.
+server(Context, Opts) ->
+    Endpoint = proplists:get_value(endpoint, Opts, "tcp://*:5556"),
+    {zerpc_server, {zerpc_server, start_link, [Endpoint, Context]}, 
+        permanent, 5000, worker, [zerpc_server]}.
 
 router() ->
     {zerpc_router, {zerpc_router, start_link, []}, permanent,
         5000, worker, [zerpc_router]}.
 
-worker_pool() ->
-    PoolSize = zerpc_util:get_env(size, 100),
+worker_pool(Opts) ->
+    PoolSize = proplists:get_value(size, Opts, 100),
     PoolArgs = [
         {name, {local, ?SERVER_POOL}},
         {worker_module, zerpc_worker},
         {size, PoolSize},
         {max_overflow, zerpc_util:get_env(overflow, PoolSize * 3)}
     ],
-    Hooks = zerpc_util:get_env(middlewares, [zerpc_mfa,zerpc_log]),
+    Hooks = proplists:get_value(middlewares, Opts, [zerpc_mfa,zerpc_log]),
     WorkerArgs = [{middlewares, Hooks}],
     poolboy:child_spec(zerpc_woker_pool, PoolArgs, WorkerArgs).
--- test/rpc_tests.erl
+++ test/rpc_tests.erl
@@ -6,6 +6,7 @@
 
 -define(ENDPOINT, "tcp://127.0.0.1:5566").
 -define(POOL_SIZE, 5).
+-define(TIMEOUT, 5000).
 
 -define(WITH_CLIENT(Count, Case), {setup,
         fun() -> start_clients(Count) end,
@@ -71,17 +72,19 @@ new_client(Name) ->
     Node.
 
 zerpc_env(Node, server) ->
-    call(Node, application, set_env, [zerpc, mode, server]),
-    call(Node, application, set_env, [zerpc, endpoint, ?ENDPOINT]),
-    call(Node, application, set_env, [zerpc, size, 5]);
+    call(Node, application, set_env, [zerpc, server, [
+                {endpoint, ?ENDPOINT},
+                {size, 5}
+            ]]);
 zerpc_env(Node, client) ->
-    call(Node, application, set_env, [zerpc, mode, client]),
-    call(Node, application, set_env, [zerpc, pools, [
-                {p1, [{endpoint, ?ENDPOINT},{size, 5}]}
+    call(Node, application, set_env, [zerpc, client, [
+                {pools, [
+                        {p1, [{endpoint, ?ENDPOINT},{size, 5}]}
+                    ]}
             ]]).
 
 call_server(Client, Fun, Args) ->
-    call(Client, zerpc, call, [p1, server, Fun, Args]).
+    call(Client, zerpc, call, [p1, server, Fun, Args, ?TIMEOUT]).
 
 call(Node, Mod, Fun, Args) ->
     rpc:call(Node, Mod, Fun, Args).
